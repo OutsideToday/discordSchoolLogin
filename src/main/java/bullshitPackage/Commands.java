f84a03008625bf33915f6a77275e13a67011523c
Brent Turner <1bctt10@gmail.com>
2022-03-04 20:10:10 -0600

    Initial commit

---
+++ src/main/java/bullshitPackage/Commands.java
@@ -0,0 +1,212 @@
+package bullshitPackage;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.text.Modal;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+
+public class Commands extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        if(event.getName().equals("bleach")){
+            String gifs[] = new String[15];
+            gifs[0] = "https://tenor.com/view/cant-unsee-how-to-unsee-blind-homer-simpson-simpsons-gif-17613101";
+            gifs[1] = "https://tenor.com/view/must-unsee-spongebob-washing-eyes-cant-look-gif-14237119";
+            gifs[2] = "https://tenor.com/view/therapy-alcohol-archer-gif-10593300";
+            gifs[3] = "https://tenor.com/view/unseen-my-eyes-delete-gif-12212414";
+            gifs[4] = "https://tenor.com/view/unsee-gif-7234566";
+            gifs[5] = "https://tenor.com/view/please-no-michael-scott-the-office-no-god-please-no-steve-carell-gif-3532257";
+            gifs[6] = "https://tenor.com/view/throw-up-dry-heave-vomit-gross-eww-gif-23254765";
+            gifs[7] = "https://tenor.com/view/bleach-clorox-gif-11117255";
+            gifs[8] = "https://tenor.com/view/mr-krabs-eyes-clean-shine-spongebob-gif-16260550";
+            gifs[9] = "https://tenor.com/view/faltas-de-ortograf%C3%ADa-eyes-eye-wash-gif-13468132";
+            gifs[10] = "https://tenor.com/view/robot-pixel-pixel-art-8bit-eye-gif-23574208";
+            gifs[11] = "https://tenor.com/view/eye-otter-rubbing-eyes-tired-irritated-gif-16638234";
+            gifs[12] = "https://tenor.com/view/unreal-no-unbelievable-steve-harvey-no-way-gif-5365038";
+            gifs[14] = "https://tenor.com/view/cleaning-windshield-wiping-squeegee-clear-view-gif-13959606";
+
+            event.reply("cleaning coming :)").queue();
+
+            for(int i = 0; i != 4; i++){
+                event.getChannel().sendMessage(gifs[(int) (Math.random() * 15)]).queue();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            event.getChannel().sendMessage("https://tenor.com/view/asian-all-clear-gaped-tooth-thumbs-up-gif-10075950").queue();
+        }
+        if(event.getName().equals("signup")){
+            TextInput name = TextInput.create("name", "Name", TextInputStyle.SHORT)
+                    .setPlaceholder("Enter your Name")
+                    .setRequired(true)
+                    .setMinLength(10)
+                    .setMaxLength(100) // or setRequiredRange(10, 100)
+                    .build();
+
+            TextInput email = TextInput.create("email", "Email", TextInputStyle.SHORT)
+                    .setPlaceholder("Enter your Email")
+                    .setRequired(true)
+                    .setMinLength(10)
+                    .setMaxLength(100)
+                    .build();
+
+            Modal modal = Modal.create("addUser", "Sign up for the Form!")
+                    .addActionRows(ActionRow.of(name), ActionRow.of(email))
+                    .build();
+
+            event.replyModal(modal).queue();
+        }
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String[] args = event.getMessage().getContentRaw().split("\\s+");
+        // INFO
+        if (args[0].equalsIgnoreCase(main.prefix + "info")) {
+            EmbedBuilder infoEmbed = new EmbedBuilder();
+            infoEmbed.setTitle("GigaChad Info!");
+            infoEmbed.setAuthor("OutsideToday");
+            infoEmbed.setColor(Color.green);
+            infoEmbed.addField("Creator", "The Creator of this bot " +
+                    "is <@124654744571478016> !", false);
+            infoEmbed.addField("Help?", "A lot of this bots success " +
+                    "can be credited to <@171086994099798016> !", false);
+            infoEmbed.addField("Commands",
+                    "Currently\n" +
+                    "/bleach\n" +
+                    "/signup", false);
+
+            event.getChannel().sendMessageEmbeds(infoEmbed.build()).queue();
+        }
+        //TIME
+        if (args[0].equalsIgnoreCase(main.prefix + "time")) {
+            Calendar time = Calendar.getInstance();
+            time.setTimeZone(TimeZone.getTimeZone("CST"));
+            String timeString = time.getTime().toString();
+
+            event.getChannel().sendMessage(timeString).queue();
+            System.out.println(timeString);
+        }
+    }
+
+    @Override
+    public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
+
+        if (event.getComponentId().equals("addUser")) {
+            TextInput name = TextInput.create("name", "Name", TextInputStyle.SHORT)
+                    .setPlaceholder("Enter your Name")
+                    .setRequired(true)
+                    .setMinLength(10)
+                    .setMaxLength(100) // or setRequiredRange(10, 100)
+                    .build();
+
+            TextInput email = TextInput.create("email", "Email", TextInputStyle.SHORT)
+                    .setPlaceholder("Enter your Email")
+                    .setRequired(true)
+                    .setMinLength(10)
+                    .setMaxLength(100)
+                    .build();
+
+            Modal modal = Modal.create("addUser", "Sign up for the Form!")
+                    .addActionRows(ActionRow.of(name), ActionRow.of(email))
+                    .build();
+
+            event.replyModal(modal).queue();
+        }
+        if (event.getComponentId().equals("removeUser")) {
+            String discordID = event.getUser().getId();
+
+            //sql shit
+            Connection conn = null;
+            PreparedStatement ptmt = null;
+            try {
+                conn = DriverManager
+                        .getConnection("jdbc:mysql://sql448.main-hosting.eu/u816645365_discordBot",
+                                "u816645365_OutsideToday",
+                                "Evopansy2*");
+                System.out.println("connected to db");
+
+                String query = "DELETE FROM classmates WHERE (discordID) = ?";
+
+                ptmt = conn.prepareStatement(query);
+
+                ptmt.setString(1, discordID);
+
+                ptmt.executeUpdate();
+
+                ptmt.close();
+                conn.close();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            //reply wehn done
+            event.reply("\nSuccessfully REMOVED!\n" + "<@" + discordID + ">").setEphemeral(false).queue();
+
+        }
+    }
+
+    @Override
+    public void onModalInteraction(@NotNull ModalInteractionEvent event) {
+        if (event.getModalId().equals("addUser")) {
+            //grabbing tha data bitch
+            String name = event.getValue("name").getAsString();
+            String email = event.getValue("email").getAsString();
+            String discordID = event.getUser().getId();
+
+            //sql shit
+            Connection conn = null;
+            PreparedStatement ptmt = null;
+            try {
+                conn = DriverManager
+                        .getConnection("jdbc:mysql://sql448.main-hosting.eu/u816645365_discordBot",
+                                "u816645365_OutsideToday",
+                                "Evopansy2*");
+                System.out.println("connected to db");
+
+                String query = "INSERT INTO classmates(discordID,name,email) VALUES(?,?,?)";
+
+                ptmt = conn.prepareStatement(query);
+
+                ptmt.setString(1, discordID);
+                ptmt.setString(2, name);
+                ptmt.setString(3, email);
+
+                ptmt.executeUpdate();
+
+                ptmt.close();
+                conn.close();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            //reply wehn done
+            event.reply("\nSuccessfully added!\n" + name).setEphemeral(false).queue();
+
+        }
+    }
+
+
+}
---
+++ src/main/java/bullshitPackage/main.java
@@ -0,0 +1,215 @@
+package bullshitPackage;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import javax.security.auth.login.LoginException;
+import java.awt.*;
+import java.sql.*;
+import java.util.ArrayList;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class main {
+    static JDA api;
+    public static String prefix = ">";
+
+    static ArrayList<String> discordIDs;
+    static ArrayList<String> emails;
+    static ArrayList<String> names;
+    static String classChannel = "945403061550075915";
+    static String class2Channel = "877931131473903676";
+    static String testChannel = "945225775148322829";
+    static TextChannel tableChannel;
+
+    /*-- CHANGE ME --*/
+    static int hour = 8;
+    static int minute = 20;
+    static boolean submitBoolean = true;
+
+
+    public static void fillForm() throws InterruptedException {
+        names = new ArrayList<>();
+        emails = new ArrayList<>();
+        discordIDs = new ArrayList<>();
+
+        //sql connection shit
+        Connection connection = null;
+        PreparedStatement pStatement = null;
+        ResultSet resultSet = null;
+        String SELECT_SQL = "SELECT * FROM classmates";
+
+        try {
+            connection = DriverManager
+                    .getConnection("jdbc:mysql://sql448.main-hosting.eu/u816645365_discordBot",
+                            "u816645365_OutsideToday",
+                            "Evopansy2*");
+            System.out.println("connected to db");
+            pStatement = connection.prepareStatement(SELECT_SQL);
+            resultSet = pStatement.executeQuery();
+
+            while(resultSet.next()){
+
+
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                String discordID = resultSet.getString("discordID");
+                names.add(name);
+                discordIDs.add(discordID);
+                emails.add(email);
+                System.out.println(name + ": added to List");
+
+            }
+            System.out.println(names);
+            System.out.println(emails);
+            System.out.println(discordIDs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
+        //chroime driver shit
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Administrator\\Desktop\\driver\\chromedriver.exe");
+        WebDriverManager.chromedriver().forceDownload();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("C:\\Program Files\\Google\\Chrome Beta\\Application\\chrome.exe");
+        WebDriver driver = new ChromeDriver(options);
+
+        //embed builder
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setTitle("Todays Submissions");
+        embedBuilder.setAuthor("OutsideToday");
+        embedBuilder.setImage("https://c.tenor.com/E0xOQmkgKE4AAAAC/betty-white-dab.gif");
+        embedBuilder.setColor(Color.CYAN);
+
+        //filling form shit per person in <names>
+        for (int i = 0; i < names.size(); i++) {
+
+            driver.get("https://forms.office.com/Pages/ResponsePage.aspx?id=ZwjbazAFD0-_iomyGPB5scW2hyWcsetPtOz3u8m5kY9UN1g4QTFPOFYwSTJMTzNSSEtYQjU5QkdXWi4u");
+            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+            WebElement noButton = driver.findElement(By.xpath("/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[2]/div[1]/div/div[2]/div/div[2]/div/label/input"));
+            WebElement emailBox = driver.findElement(By.xpath("//*[@id=\"form-container\"]/div/div/div[1]/div/div[1]/div[2]/div[2]/div[2]/div/div[2]/div/div/input"));
+            WebElement submitButton = driver.findElement(By.xpath("/html/body/div/div/div/div/div[1]/div/div[1]/div[2]/div[3]/div[1]/button"));
+
+            noButton.click();
+            Thread.sleep((long) (Math.random()*1000));
+
+            emailBox.click();
+            emailBox.clear();
+            emailBox.sendKeys(emails.get(i));
+            Thread.sleep((long) (Math.random()*1000));
+
+
+
+            if(submitBoolean == true){
+                //submitButton.click(); //submit the form
+            }
+
+            System.out.println("SUBMITTED FOR " + names.get(i));
+
+            embedBuilder.addField("Name", names.get(i), false);
+
+            driver.navigate().refresh();
+        }
+        //output to defined text chanel
+        if(submitBoolean == true) {
+           // tableChannel = api.getTextChannelById(classChannel);
+           // tableChannel.sendMessageEmbeds(embedBuilder.build())
+           //         .setActionRow(Button.secondary("addUser", "Sign Up!"), Button.danger("removeUser", "Remove Me"))
+            //        .queue();
+
+            tableChannel = api.getTextChannelById(class2Channel);
+            tableChannel.sendMessageEmbeds(embedBuilder.build())
+                    .setActionRow(Button.secondary("addUser", "Sign Up!"), Button.danger("removeUser", "Remove Me"))
+                    .queue();
+        }else if(submitBoolean == false){
+            tableChannel = api.getTextChannelById(testChannel);
+        }
+
+
+        driver.quit();
+    }
+    static class getSleepTime implements Runnable{
+
+        @Override
+        public void run() {
+            while(true) {
+                // get current time
+                Calendar targetCal = Calendar.getInstance(TimeZone.getTimeZone("CST"));
+
+                // check if current time is before 8:20am
+                Calendar today820 = Calendar.getInstance();
+                today820.set(Calendar.HOUR_OF_DAY, hour);
+                today820.set(Calendar.MINUTE, minute);
+
+                // unless it's before 8:20a today, add 1 day for target time
+                if (targetCal.after(today820)) {
+                    targetCal.add(Calendar.DATE, 1);
+                }
+
+                // set time to tomorrow at 8:20am
+                targetCal.set(Calendar.HOUR_OF_DAY, hour);
+                targetCal.set(Calendar.MINUTE, minute);
+
+                // add another day until we get to next day that is M-F
+                while (targetCal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY ||
+                        targetCal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
+
+                    targetCal.add(Calendar.DATE, 1);
+
+                }
+
+                // get time between now and target time
+                Calendar nowCal = Calendar.getInstance(TimeZone.getTimeZone("CST"));
+
+                long sleepTime = targetCal.getTimeInMillis() - nowCal.getTimeInMillis();
+
+                int hours = (int) ((sleepTime / (1000 * 60 * 60)) % 24);
+
+                System.out.println("hours till " + hour + ":" + minute + " " + hours);
+
+                // return dat
+                try {
+                    Thread.sleep(sleepTime);
+
+                    fillForm();
+
+                    Thread.sleep(60000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws LoginException, InterruptedException{
+        final String TOKEN = "ODY4MzQ5NTU3NDg0OTc4MTk2.YPuXiw.MZ0JWO5gY7oodIhI8ZjoXWAD0gU";
+
+        api = JDABuilder.createDefault(TOKEN).build();
+
+        api.awaitReady();
+
+        api.getPresence().setActivity(Activity.playing(">info"));
+
+        api.addEventListener(new Commands());
+        api.upsertCommand("bleach", "clean yo screen of the filth FOOL, Matt you also a bitch").queue();
+        api.upsertCommand("signup", "Sign up for the submissions :)").queue();
+
+        Thread sleepFormThread = new Thread(new getSleepTime());
+
+        sleepFormThread.start();
+
+    }
+}
